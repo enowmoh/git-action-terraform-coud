111690ebfd50c7c093b0d994aea33b1eab529b92
Enow-moh <116763125+enowmoh@users.noreply.github.com>
2023-01-26 18:35:54 -0600

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -12,3 +12,25 @@ variable "my_instance_type"{
   type = string
   default = "t2.micro"
 }
+variable "name_input" {
+    description = "instance name hannah"
+    type = string
+    default = "hannah"
+    }
+
+variable "region" {
+    description = "the aws region"
+    type = string
+    default = "us-east-2"
+    }
+
+variable "my_ami" {
+    description = "aws ami"
+    type = string
+    default = "ami-05bfbece1ed5beb54"
+    }
+variable "key" {
+    description = "aws key"
+    type = string
+    default = "key jenkins"
+    }
